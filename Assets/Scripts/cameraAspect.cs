5581a916bd78370050e31aba927fa0a34643df0c
DarthSaffa <rowen.koortzen@gmail.com>
2016-09-07 17:01:44 +1000

    Dragon movement and camera controls
    Did the basic movement controls
    The movement restrictions
    And the sexy ass letterbox effect

---
+++ Assets/Scripts/cameraAspect.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraAspect : MonoBehaviour {
+
+	public Camera letterbox;
+	public Camera MainCamera;
+
+	// Update is called once per frame
+	void Update () {
+		
+		// Maintain the 9:16 aspectratio using letterboxing (vertical or horisontal depending screen dimensions)
+		if (letterbox.aspect < 0.9f)
+		{
+			MainCamera.rect = new Rect(0f, (1.0f - letterbox.aspect / 0.9f) / 2.0f, 1.6f, letterbox.aspect / 0.9f);
+		}
+		else
+		{
+			MainCamera.rect = new Rect((1.0f - 0.9f / letterbox.aspect) / 2.0f, 0, 0.9f / letterbox.aspect, 1.6f);
+		}
+ 	}
+}
---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+	Transform myTransform;
+	Rigidbody2D rb;
+	int speed = 5;
+
+	//Camera
+	public Camera cam;
+	float camHeight;
+	float camWidth;
+
+	// Use this for initialization
+	void Start () {
+	
+		myTransform = transform;
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		//Movement
+		rb.velocity = new Vector2 (Input.GetAxisRaw("Horizontal")*speed, Input.GetAxisRaw("Vertical")*speed);
+
+		//Setting camera constraint variables
+		camHeight = 2* cam.orthographicSize;
+		camWidth = camHeight * cam.aspect;
+
+		//Camera constraints
+		//X axis camera constraints
+		if(myTransform.position.x > camWidth/2 - camWidth/40){
+			myTransform.position = new Vector2 (camWidth/2 - camWidth/40,myTransform.position.y);
+		}
+		if(myTransform.position.x < -camWidth/2 + camWidth/40){
+			myTransform.position = new Vector2 (-camWidth/2 + camWidth/40,myTransform.position.y);
+		}
+
+		//Y axis camera constraints
+		if(myTransform.position.y > camHeight-camHeight/13){
+			myTransform.position = new Vector2 (myTransform.position.x,camHeight-camHeight/13);
+		}
+		if(myTransform.position.y < camHeight/30){
+			myTransform.position = new Vector2 (myTransform.position.x,camHeight/30);
+		}
+	}
+}
