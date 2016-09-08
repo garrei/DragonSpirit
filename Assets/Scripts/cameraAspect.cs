4afe02f7ee27318171278cac4373c8635b2e8adc
DarthSaffa <rowen.koortzen@gmail.com>
2016-09-09 01:32:44 +1000

    Bug fixes
    Fixed some issues with the movement restrictions and replaced some
    textured

--- Assets/Scripts/cameraAspect.cs
+++ Assets/Scripts/cameraAspect.cs
@@ -8,15 +8,27 @@ public class CameraAspect : MonoBehaviour {
 
 		// Update is called once per frame
 	void Update () {
-
-		// Maintain the 9:16 aspectratio using letterboxing (vertical or horisontal depending screen dimensions)
+		
+		/*
+		// Maintain the aspect ratio using letterboxing (vertical or horizontal depending screen dimentions)
 		if (letterbox.aspect < 0.9f)
 		{
-			mainCamera.rect = new Rect(0f, (1.0f - letterbox.aspect / 0.9f) / 2.0f, 1.6f, letterbox.aspect / 0.9f);
+			mainCamera.rect = new Rect(0f, (1.0f - letterbox.aspect / 0.9f) / 2, 1.6f, letterbox.aspect / 0.9f);
 		}
 		else
 		{
-			mainCamera.rect = new Rect((1.0f - 0.9f / letterbox.aspect) / 2.0f, 0, 0.9f / letterbox.aspect, 1.6f);
+			mainCamera.rect = new Rect((1.0f - 0.9f / letterbox.aspect) / 2, 0, 0.9f / letterbox.aspect, 1.6f);
 		}
+		*/
+		if (letterbox.aspect < 0.7777777f)
+		{
+			mainCamera.rect = new Rect(0f, (1f - letterbox.aspect / 0.7777777f) / 2.0f, 1f, letterbox.aspect / 0.7777777f);
+		}
+		else
+		{
+			mainCamera.rect = new Rect((1f - 0.7777777f / letterbox.aspect) / 2.0f, 0, 0.7777777f / letterbox.aspect, 1f);
+		}
+
+
  	}
 }
