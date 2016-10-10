a125a02886335e08e2a38b435d4390d81bbaf49b
DarthSaffa <rowen.koortzen@gmail.com>
2016-10-10 14:13:22 +1000

    Fixed the shit
    shit the Fixed

--- Assets/Scripts/Enemies/RowenAI/KopteraBehaviour.cs
+++ Assets/Scripts/Enemies/RowenAI/KopteraBehaviour.cs
@@ -7,8 +7,11 @@ public class KopteraBehaviour : MonoBehaviour {
 	Transform myTransform;
 	int speedVertical = 7;
 	int speedHorizontal = 2;
+	float rangeVertical = 2;
+	float rangeHorizontal = 1.5f;
 	Vector3 moveDirectionVertical = Vector3.down;
 	Vector3 moveDirectionHorizontal = Vector3.right;
+	SpriteRenderer mySprite;
 
 	// Use this for initialization
 	void Start () {
@@ -16,18 +19,21 @@ public class KopteraBehaviour : MonoBehaviour {
 		playerTransform = GameObject.Find ("PlayerGraphic").transform;
 
 		myTransform = transform;
+
+		mySprite = GetComponent <SpriteRenderer> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-		if (myTransform.position.y < playerTransform.position.y + 1.5) {
+		if (myTransform.position.y < playerTransform.position.y + rangeVertical && myTransform.position.x < playerTransform.position.x + rangeHorizontal && myTransform.position.x > playerTransform.position.x - rangeHorizontal && myTransform.position.y > playerTransform.position.y) {
 			moveDirectionVertical = Vector3.up;
+			mySprite.flipY = false;
 		}
-		if (myTransform.position.x < playerTransform.position.x + 1.5 && moveDirectionVertical == Vector3.down) {
+		if (myTransform.position.x < playerTransform.position.x + rangeHorizontal && moveDirectionVertical == Vector3.down) {
 			moveDirectionHorizontal = Vector3.left;
 		}
-		if (myTransform.position.x < playerTransform.position.x - 1.5 && moveDirectionVertical == Vector3.down) {
+		if (myTransform.position.x < playerTransform.position.x - rangeHorizontal && moveDirectionVertical == Vector3.down) {
 			moveDirectionHorizontal = Vector3.right;
 		}
 
