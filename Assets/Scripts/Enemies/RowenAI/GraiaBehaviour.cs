6c28e9c5e02908ecf83f8c93c41720831f1be3e5
DarthSaffa <rowen.koortzen@gmail.com>
2016-10-12 14:11:33 +1000

    Started animating Graia and attempted to fix CameraAspect name
    pls work pls

--- Assets/Scripts/Enemies/RowenAI/GraiaBehaviour.cs
+++ Assets/Scripts/Enemies/RowenAI/GraiaBehaviour.cs
@@ -8,7 +8,8 @@ public class GraiaBehaviour : MonoBehaviour {
 	int speedVertical = 7;
 	int speedHorizontal = 2;
 	float rangeHorizontal = 1.5f;
-	Vector3 moveDirectionHorizontal = Vector3.right;
+	Vector3 moveDirectionHorizontal = new Vector2 (0,0);
+	SpriteRenderer mySprite;
 
 	// Use this for initialization
 	void Start () {
@@ -16,19 +17,25 @@ public class GraiaBehaviour : MonoBehaviour {
 		playerTransform = GameObject.Find ("PlayerGraphic").transform;
 
 		myTransform = transform;
+
+		//We need to grab the sprite so we can flip it later
+		mySprite = GetComponent <SpriteRenderer> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
 
+		//This moves the transform left and right and flips the sprite accordingly
 		if (myTransform.position.x < playerTransform.position.x + rangeHorizontal && myTransform.position.y > playerTransform.position.y) {
-			moveDirectionHorizontal = Vector3.left;
+			moveDirectionHorizontal = Vector2.left;
+			mySprite.flipX = false;
 		}
 		if (myTransform.position.x < playerTransform.position.x - rangeHorizontal && myTransform.position.y > playerTransform.position.y) {
-			moveDirectionHorizontal = Vector3.right;
+			moveDirectionHorizontal = Vector2.right;
+			mySprite.flipX = true;
 		}
 
-		myTransform.Translate (Vector3.down* Time.deltaTime * speedVertical);
+		myTransform.Translate (Vector2.down* Time.deltaTime * speedVertical);
 		myTransform.Translate (moveDirectionHorizontal * Time.deltaTime * speedHorizontal);
 	}
 }
