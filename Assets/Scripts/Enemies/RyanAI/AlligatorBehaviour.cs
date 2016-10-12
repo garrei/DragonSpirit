b8bce301f5ea5405ba4556c0e7c506c7e79607bc
Ryan <3106722708@student.tafe>
2016-10-12 14:07:01 +1000

    Ryan Changed some thing
    WOw so changhed

--- Assets/Scripts/Enemies/RyanAI/AlligatorBehaviour.cs
+++ Assets/Scripts/Enemies/RyanAI/AlligatorBehaviour.cs
@@ -3,13 +3,37 @@ using System.Collections;
 
 public class AlligatorBehaviour : MonoBehaviour {
 
+	Transform playerTransform;
+	Transform myTransform;
+	int speedVertical = 7;
+	int speedHorizontal = 2;
+	float rangeVertical = 2;
+	float rangeHorizontal = 1.5f;
+	Vector3 moveDirectionVertical = Vector3.down;
+	Vector3 moveDirectionHorizontal = Vector3.right;
+	SpriteRenderer mySprite;
+
 	// Use this for initialization
 	void Start () {
-	
+		//Finding the player this way should make it more compatable between scenes
+		playerTransform = GameObject.Find ("PlayerGraphic").transform;
+
+		myTransform = transform;
+
+		mySprite = GetComponent <SpriteRenderer> ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+
+		if (myTransform.position.y < playerTransform.position.y + rangeVertical && myTransform.position.x < playerTransform.position.x + rangeHorizontal && myTransform.position.x > playerTransform.position.x - rangeHorizontal && myTransform.position.y > playerTransform.position.y) {
+			moveDirectionVertical = Vector3.down;
+		}
+		if (myTransform.position.y > playerTransform.position.y + rangeVertical && myTransform.position.x < playerTransform.position.x + rangeHorizontal && myTransform.position.x > playerTransform.position.x - rangeHorizontal && myTransform.position.y > playerTransform.position.y) {
+			speedVertical = 0;
+		}
+
+		myTransform.Translate (moveDirectionVertical * Time.deltaTime * speedVertical);
+		myTransform.Translate (moveDirectionHorizontal * Time.deltaTime * speedHorizontal);
 	}
 }
