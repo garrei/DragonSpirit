4a09101e20f970a1c4ff1037621e930d0e8f91c3
DarthSaffa <rowen.koortzen@gmail.com>
2016-10-13 18:23:53 +1000

    Added and enemyspawner and player health
    They are both very simple, but get the job done
    
    The spawner is supa fancy tho

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject enemyToSpawn;
+	SpriteRenderer mySpriteRenderer;
+	SpriteRenderer enemySpriteRenderer;
+	Transform myTransform;
+
+	// Use this for initialization
+	void Start () {
+		myTransform = transform;
+
+		//Using a SpriteRenderer to visually show what enemy is being spawned
+		mySpriteRenderer = GetComponent <SpriteRenderer> ();
+
+		//We need to find the SpriteRenderer of the enemy to show what is being spawned
+		enemySpriteRenderer = enemyToSpawn.GetComponent<SpriteRenderer> ();
+
+		mySpriteRenderer.sprite = enemySpriteRenderer.sprite;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(myTransform.position.y < 7){
+			Instantiate (enemyToSpawn, myTransform.position, Quaternion.identity);
+			Destroy (gameObject);
+		}
+	
+	}
+}
