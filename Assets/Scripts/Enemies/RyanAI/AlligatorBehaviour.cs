8ba1dd9074a566b17ea6ff1ccdf082febfe5adb6
Ryan <nirox42@gmail.com>
2016-10-13 18:49:01 +1000

    Fixed the GIT
    i hope

--- Assets/Scripts/Enemies/RyanAI/AlligatorBehaviour.cs
+++ Assets/Scripts/Enemies/RyanAI/AlligatorBehaviour.cs
@@ -5,18 +5,13 @@ public class AlligatorBehaviour : MonoBehaviour {
 
 	Transform playerTransform;
 	Transform myTransform;
-<<<<<<< HEAD
+
 	public GameObject alligatorProjectile;
 	int speedVertical = 2;
 	int speedHorizontal = 0;
 	float rangeVertical = 4;
 	float rangeHorizontal = 10f;
-=======
-	int speedVertical = 7;
-	int speedHorizontal = 2;
-	float rangeVertical = 2;
-	float rangeHorizontal = 1.5f;
->>>>>>> origin/master
+
 	Vector3 moveDirectionVertical = Vector3.down;
 	Vector3 moveDirectionHorizontal = Vector3.right;
 	SpriteRenderer mySprite;
@@ -35,7 +30,7 @@ public class AlligatorBehaviour : MonoBehaviour {
 	void Update () {
 
 		if (myTransform.position.y < playerTransform.position.y + rangeVertical && myTransform.position.x < playerTransform.position.x + rangeHorizontal && myTransform.position.x > playerTransform.position.x - rangeHorizontal && myTransform.position.y > playerTransform.position.y) {
-<<<<<<< HEAD
+
 			speedVertical = 0;
 			Attack();
 			mySprite.flipY = false;
@@ -44,28 +39,12 @@ public class AlligatorBehaviour : MonoBehaviour {
 			speedVertical = 2;
 			mySprite.flipY = false;
 		}
-		if (myTransform.position.x < playerTransform.position.x + rangeHorizontal && moveDirectionVertical == Vector3.down) {
-			moveDirectionHorizontal = Vector3.left;
-		}
-		if (myTransform.position.x < playerTransform.position.x - rangeHorizontal && moveDirectionVertical == Vector3.down) {
-			moveDirectionHorizontal = Vector3.right;
-=======
-			moveDirectionVertical = Vector3.down;
-		}
-		if (myTransform.position.y > playerTransform.position.y + rangeVertical && myTransform.position.x < playerTransform.position.x + rangeHorizontal && myTransform.position.x > playerTransform.position.x - rangeHorizontal && myTransform.position.y > playerTransform.position.y) {
-			speedVertical = 0;
->>>>>>> origin/master
-		}
 
 		myTransform.Translate (moveDirectionVertical * Time.deltaTime * speedVertical);
-		myTransform.Translate (moveDirectionHorizontal * Time.deltaTime * speedHorizontal);
-<<<<<<< HEAD
 	}
 
 	void Attack ()
 	{
-		
-=======
->>>>>>> origin/master
+
 	}
 }
