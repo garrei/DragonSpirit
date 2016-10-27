043f542b900476ab5365bfbc31d5e6f40d3f3437
DarthSaffa <rowen.koortzen@gmail.com>
2016-10-27 15:00:15 +1000

    Terrain movement is perfect now and works with boss
    Time to place some enemies

--- Assets/Scripts/TerrainMove.cs
+++ Assets/Scripts/TerrainMove.cs
@@ -1,28 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TerrainMove : MonoBehaviour {
+public class PlesiosaurusMove : MonoBehaviour {
 
 	bool terrainIsMoving = true;
 	float speed = 0.5f;
 	Transform myTransform;
-	public GameObject player;
+	GameObject player;
 
 	// Use this for initialization
 	void Start () {
 		myTransform = transform;
+		player = GameObject.Find ("PlayerGraphic");
 	}
 
 	// Update is called once per frame
 	void Update () {
 
 		//Object moves down at a constant speed
-		if(terrainIsMoving == true){
+		if(terrainIsMoving == true && myTransform.position.y > 1){
 			myTransform.Translate (Vector3.down *speed * Time.deltaTime);
+		}
+
+		if(myTransform.position.y < 1){
+			myTransform.position = new Vector2 (myTransform.position.x, 1);
 		}
 
 		//This moves the terrain in the opposite direction that the player is moving
 		//Used the negative of the player's position to stay consistent
-		myTransform.position = new Vector3(-player.transform.position.x/4,myTransform.position.y,myTransform.position.z);
+		myTransform.position = new Vector3(-player.transform.position.x/2.6f,myTransform.position.y,myTransform.position.z);
 	}
 }
