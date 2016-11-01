b8492646f94a4a26f3f4c9b75ae9db80140eacbc
DarthSaffa <rowen.koortzen@gmail.com>
2016-11-01 19:18:38 +1000

    Fucked with the UI and Changed green egg to purple

---
+++ Assets/Scripts/UIHUD.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIHUD : MonoBehaviour {
+
+	public string uiType = "Fire";
+	int uiNumber = 2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(uiType == "Fire"){
+
+		}
+	
+	}
+}
