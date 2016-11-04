d314c233223187a0bd3d77aa7548d3974fe59ec7
Ryan <nirox42@gmail.com>
2016-11-04 19:35:35 +1000

    Sparkler Enemy Ready

---
+++ Assets/Scripts/Enemies/RyanAI/SparklerBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SparklerBehaviour : MonoBehaviour {
+
+	Transform playerTransform;
+	Transform myTransform;
+	float speedVertical = 1;
+	float speedHorizontal = 4;
+	Vector3 moveDirectionVertical = Vector3.up;
+	Vector3 moveDirectionHorizontal = Vector3.right;
+
+	// Use this for initialization
+	void Start () 
+	{
+		myTransform = transform;
+		playerTransform = GameObject.Find ("PlayerGraphic").transform;
+		if (myTransform.position.x < playerTransform.position.x) 
+		{
+			moveDirectionHorizontal = Vector3.right;
+		}
+		else if (myTransform.position.x > playerTransform.position.x)
+		{
+			moveDirectionHorizontal = Vector3.left;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		speedVertical = speedVertical + Time.deltaTime * 2f;
+		speedHorizontal = speedHorizontal - Time.deltaTime * 2f;
+
+
+
+		myTransform.Translate (moveDirectionVertical * Time.deltaTime * speedVertical);
+		myTransform.Translate (moveDirectionHorizontal * Time.deltaTime * speedHorizontal);
+	
+	}
+}
